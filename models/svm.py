1bbf536c5ced438b51310690d85ee044a0d06eee
RafiaMaqbool <125145528+RafiaMaqbool@users.noreply.github.com>
2023-12-11 19:24:56 +0500

    Update svm.py

--- models/svm.py
+++ models/svm.py
@@ -5,7 +5,7 @@ from sklearn.svm import SVC
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, classification_report
 
-# Load the dataset
+# Load Dataset
 file_path = '/content/pakistani_dataset_consolidated_augmented.xlsx'
 data = pd.read_excel(file_path)
 
@@ -22,16 +22,17 @@ if 'Textual Rating' in data.columns:
     label_encoder = LabelEncoder()
     data['Textual Rating'] = label_encoder.fit_transform(data['Textual Rating'])
 
+    # Convert text data into TF-IDF features
     tfidf = TfidfVectorizer(stop_words='english', max_features=5000, ngram_range=(1, 2))
     X = tfidf.fit_transform(data['Text'])
     y = data['Textual Rating']
 
     # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(
-        X, y, data.index, test_size=0.2, random_state=42
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
     )
 
-    # Initialize and train the SVM model
+    # Initialize and train the SVM model for multi-class classification
     model = SVC(kernel='linear', C=1.0)
     model.fit(X_train, y_train)
 
@@ -39,16 +40,16 @@ if 'Textual Rating' in data.columns:
     y_pred = model.predict(X_test)
 
     # Display news with their predicted labels after testing
-    for i in range(len(indices_test)):
-        idx = indices_test[i]
-        print(f"Text: {data['Text'][idx]}\nPredicted Label: {label_encoder.inverse_transform([y_pred[i]])[0]}\n")
+    for i in range(len(y_test)):
+        index = y_test.index[i]
+        print(f"Text: {data['Text'].iloc[index]}\nPredicted Label: {label_encoder.inverse_transform([y_pred[i]])[0]}\n")
 
     # Calculate accuracy
     accuracy = accuracy_score(y_test, y_pred)
     print(f"Accuracy: {accuracy:.2f}")
 
     # Additional evaluation metrics
-    print('Classification Report:\n',classification_report(y_test, y_pred, zero_division=1))
+    print('Classification Report:\n', classification_report(y_test, y_pred, zero_division=1))
 
 else:
     print("Column 'Textual Rating' not found in the dataset.")
